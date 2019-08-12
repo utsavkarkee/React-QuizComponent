3904807d2d6c452ee7088cffd5be62374d7cd4f4
Utsav Karkee <utsav.karkee@yahoo.com>
2019-08-12 08:23:11 +0530

    Iterative QuizQuestions and Click Handling

--- src/Quiz.js
+++ src/Quiz.js
@@ -18,10 +18,16 @@ class Quiz extends Component {
             <div>
                 {isQuizEnd 
                     ? <QuizEnd />
-                    : <QuizQuestion quiz_question
-                        ={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
+                    : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
+                        showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
             </div>
         );
+    }
+
+    showNextQuestion() {
+        this.setState((state) => {
+            return { quiz_position: state.quiz_position + 1 };
+        });
     }
 }
 
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -7,15 +7,24 @@ class QuizQuestion extends Component {
         return (
             <main>
                 <section>
-                <p>{this.props.quiz_question.instruction_text}</p>
+                    <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
                 <section className="buttons">
-                <ul>
-                    <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
-                </ul>
+                    <ul>
+                        {this.props.quiz_question.answer_options.map((answer_option, index) => {
+                            return <QuizQuestionButton key={index} button_text={answer_option}
+                                clickHandler={this.handleClick.bind(this)} />
+                        })}
+                    </ul>
                 </section>
             </main>
         );
+    }
+
+    handleClick(buttonText) {
+        if (buttonText === this.props.quiz_question.answer) {
+            this.props.showNextQuestionHandler();
+        }
     }
 }
 
