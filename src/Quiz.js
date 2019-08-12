4858eaa92982f8e14612b142e2316f4f8225a8ec
Utsav Karkee <utsav.karkee@yahoo.com>
2019-08-12 08:44:30 +0530

    Functionality to reset the quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -17,7 +17,7 @@ class Quiz extends Component {
         return (
             <div>
                 {isQuizEnd 
-                    ? <QuizEnd />
+                    ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
                     : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
                         showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
             </div>
@@ -28,6 +28,10 @@ class Quiz extends Component {
         this.setState((state) => {
             return { quiz_position: state.quiz_position + 1 };
         });
+    }
+
+    handleResetClick() {
+        this.setState({ quiz_position: 1 });
     }
 }
 
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,9 +5,7 @@ class QuizQuestion extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            incorrectAnswer: false
-        }
+        this.state = { incorrectAnswer: false }
     }
 
     render() {
@@ -34,14 +32,10 @@ class QuizQuestion extends Component {
 
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
-            this.setState((state) => {
-                return { incorrectAnswer: false };
-            });
+            this.setState({ incorrectAnswer: false });
             this.props.showNextQuestionHandler();
         } else {
-            this.setState((state) => {
-                return { incorrectAnswer: true };
-            });
+            this.setState({ incorrectAnswer: true });
         }
     }
 }
